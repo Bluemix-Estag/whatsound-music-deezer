bc6d21e61617c62c638f52132a525d84f7d0943c
carloslobato03 <carloslobato03@gmail.com>
2017-06-11 01:46:01 -0300

    Request to get info about a track from track id

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.get('/whatsound/api/v1/deezer/track/values', function (req, res) {
             var musics = [];
             if (!error && response.statusCode == 200) {
                 var info = JSON.parse(body);
+                console.log(info);
                 if (info != ' ') {
                     if (info['data'].length == 0) {
                         var result = {
@@ -72,6 +73,71 @@ app.get('/whatsound/api/v1/deezer/track/values', function (req, res) {
             }
         }
         request(options, callback);
+    }
+});
+app.get('/whatsound/api/v1/deezer/track/info/values', function (req, res) {
+    
+    res.setHeader('Content-Type','application/json');
+    
+    var query = req.query.query;
+    
+    var out = JSON.stringify(req.query.query);
+    var out2 = out.replace(/[^0-9]/g, ''); 
+    var trackUri = parseInt(out2);
+    
+    query = (query != null && query != '' && query != "undefined") ? query : null;
+    // tipo = tracks, artists, albums
+    if (query == null) {
+        var result = {
+            status: false,
+            message: "Bad request, {Empty query}"
+        }
+        res.status(400).json(result);
+    } else {
+        var optionsInfo = {
+            url: "https://api.deezer.com/track/" + trackUri,
+            headers: {
+                Accept: 'text/json'
+            }
+        };
+        function callbackInfo(error, response, body) {
+            var musics = [];
+            if (!error && response.statusCode == 200) {
+                var infoDeezer = JSON.parse(body);
+                if (infoDeezer != ' ') {
+                    if (infoDeezer.length == 0) {
+                        var result = {
+                            "code": 200,
+                            "message": "Music info not found",
+                            "status": false
+                        }
+                        res.status(200).json(result);
+                    } else {
+                                var resultDeezer = {
+                                    "name": JSON.stringify(infoDeezer['title_short']).replace(new RegExp('\\"', "g"), ""),
+                                    "track_id": (infoDeezer['id']),
+                                    "duration": infoDeezer['duration'],
+                                    "artist": JSON.stringify(infoDeezer['artist']['name']).replace(new RegExp('\\"', "g"), ""),
+                                    "artist_id": (infoDeezer['artist']['id']),
+                                    "artist_image": JSON.stringify(infoDeezer['artist']['picture_xl']).replace(new RegExp('\\"', "g"), ""),
+                                    "artist_tracklist": JSON.stringify(infoDeezer['artist']['tracklist']).replace(new RegExp('\\"', "g"), ""),
+                                    "album": JSON.stringify(infoDeezer['album']['title']).replace(new RegExp('\\"', "g"), ""),
+                                    "album_id": (infoDeezer['album']['id']),
+                                    "album_image": JSON.stringify(infoDeezer['album']['cover_xl']).replace(new RegExp('\\"', "g"), ""),
+                                    "album_tracklist": JSON.stringify(infoDeezer['album']['tracklist']).replace(new RegExp('\\"', "g"), ""),
+                                    "track_url": JSON.stringify(infoDeezer['link']).replace(new RegExp('\\"', "g"), "")
+                                }
+                          
+                        res.status(200).json(resultDeezer);
+                    }
+                }else{
+                    res.status(500).json({status: false, message : "Internal Server Error" , code : 500});
+                }
+            } else {
+                res.status(response.statusCode).json(body);
+            }
+        }
+        request(optionsInfo, callbackInfo);
     }
 });
 //serve static file (index.html, images, css)
